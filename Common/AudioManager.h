99cc5c9c4904994bf5994cce3fadd48c39fdd508
lukegong77 <74024569+lukegong77@users.noreply.github.com>
2021-03-23 15:03:14 +0000

    3D sound can be added in a specific position

--- Common/AudioManager.h
+++ Common/AudioManager.h
@@ -1,23 +1,30 @@
 #pragma once
+#include <windows.h>
 #include <irrKlang.h>
 #include <stdio.h>
 #include <conio.h>
 #include <string>
 #include <vector>
+#include "Vector3.h"
 using namespace irrklang;
 
 class AudioManager
 {
 public:
-	AudioManager();
+	AudioManager(); 
+	void SetPlayerPos(vec3df PlayerPos);
 	void PlayAudio(std::string dir, bool loop = false);
 	void UpdateAudio(float dt);
 	void StopSound();
+	void Play3DAudio(std::string dir, const PxTransform& t, bool loop);
+
 	static int& GetVolume() {
 		return volume;
 	}
 private:
 	ISoundEngine* engine;
+	ISound* music;
+	vec3df ListenerPos;
 	static int volume;
 };
 
